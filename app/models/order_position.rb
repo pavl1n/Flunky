3815a8a7cf34d4f02f607b8f8c9e1c283a83509f
pavel_shymanski <pavl1n>
2021-04-09 16:39:04 +0300

    comments for models

--- app/models/order_position.rb
+++ app/models/order_position.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: false
+
+# OrderPosition connects product and restaurant_order association m2m
 class OrderPosition < ApplicationRecord
   belongs_to :product
   belongs_to :restaurant_order
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: false
 
+# Model for product entity
 class Product < ApplicationRecord
   belongs_to :restaurant, -> { where user_type: :restaurant }, class_name: 'User', foreign_key: 'user_id'
   has_many :order_positions
