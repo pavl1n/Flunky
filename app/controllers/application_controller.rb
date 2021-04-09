16048d82a914f14230477f1ec576f690cf9414d0
pavel_shymanski <pavl1n>
2021-04-09 13:37:19 +0300

    added configs for reek, rubocop and hound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: false
+
+# This module smells of :reek:IrresponsibleModule
 class ApplicationController < ActionController::Base
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: false
+
+# This module smells of :reek:IrresponsibleModule
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
