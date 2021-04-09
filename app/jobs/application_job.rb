bf9baddaae40075261a59ab3154913903325b55b
pavel_shymanski <pavl1n>
2021-04-09 13:37:19 +0300

    added configs for reek, rubocop and hound

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: false
+
+# This module smells of :reek:IrresponsibleModule
 class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
