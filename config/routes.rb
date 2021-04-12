6c92f73ce85d73cdd5a396af1d1f88c51abd18cd
pavel_shymanski <pavl1n>
2021-04-12 19:49:27 +0300

    configured devise and view for registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'menu#index'
   get 'users/profile'
   devise_for :users
+  root to: 'users#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
