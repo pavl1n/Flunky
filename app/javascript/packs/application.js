a8dca5c49688998ff35046ab4b5094df41cf9366
pavel_shymanski <pavl1n>
2021-04-15 19:31:57 +0300

    fixed hound

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,6 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-require jquery3
-require popper
-require bootstrap-sprockets
+jquery3.start();
+popper.start();
+bootstrap-sprockets.start();
