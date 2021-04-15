e370ca47a604fa83204de92f255f2ff914f2eb98
pavel_shymanski <pavl1n>
2021-04-15 20:33:29 +0300

    changed registration from email to phone number

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'menu#index'
   get 'users/profile'
   devise_for :users
-  root 'main_page#index'
+  root 'users#profile' # CHANGE!!!!!!
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
