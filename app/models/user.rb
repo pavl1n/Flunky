3e2f00d0f63808b53de6ebfa7922e70cc73c537f
pavel_shymanski <pavl1n>
2021-04-15 15:45:16 +0300

    added validation on update

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
 
   has_many :products
   has_many :orders
-  # validates :name, :phone_number, presence: true
+  validates :name, presence: true, on: :update
+  validates :phone_number, presence: true, on: :update
   enum user_type: { admin: 0, client: 1, restaurant: 2 }
 
   validates :phone_number, uniqueness: true, phone: { possible: true, types: :mobile, countries: :by }
