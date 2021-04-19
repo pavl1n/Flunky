161a0408e5bc882fae397e5c4999f11d8662fcb0
pavel_shymanski <pavl1n>
2021-04-19 11:40:35 +0300

    made authentification + added field in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_04_15_061219) do
-=======
-ActiveRecord::Schema.define(version: 2021_04_12_091412) do
->>>>>>> 120ab5ff (init devise gem)
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_091412) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "city"
-    t.string "phone_number", null: false
+    t.string "phone_number"
     t.integer "user_type", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -72,21 +68,10 @@ ActiveRecord::Schema.define(version: 2021_04_12_091412) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-<<<<<<< HEAD
-<<<<<<< HEAD
     t.boolean "confirmed", default: false
-=======
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
->>>>>>> a82b9f06 (changed migrations)
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-=======
->>>>>>> 51ac97e8 (added phone confirmation)
   end
 
 end
