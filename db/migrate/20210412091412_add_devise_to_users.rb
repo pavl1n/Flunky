dc1492e273cf4b57cfd16e2c568a69f4b3c2206e
pavel_shymanski <pavl1n>
2021-04-19 12:35:25 +0300

    changed default of confirmed on false

--- db/migrate/20210412091412_add_devise_to_users.rb
+++ db/migrate/20210412091412_add_devise_to_users.rb
@@ -8,11 +8,11 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
       t.datetime :remember_created_at
-      t.boolean :confirmed, default: ""
+      t.boolean :confirmed, default: false
     end
   end
 
   def self.down
-    raise ActiveRecord::IrreversibleMigration
+    #raise ActiveRecord::IrreversibleMigration
   end
 end
