0bd180d5f79c43048921837db9650afe650dca8d
pavel_shymanski <pavl1n>
2021-04-19 10:24:48 +0300

    added phone confirmation

---
+++ app/controllers/phone_verifications_controller.rb
@@ -0,0 +1,41 @@
+class PhoneVerificationsController < ApplicationController
+  def new
+  end
+
+  def create
+    session[:phone_number] = params[:phone_number]
+    session[:country_code] = params[:country_code]
+    @response = Authy::PhoneVerification.start(
+      via: params[:method],
+      country_code: '+375',
+      phone_number: params[:phone_number]
+    )
+    if @response.ok?
+      redirect_to challenge_phone_verifications_path
+    else
+      render :new
+    end
+
+  end
+
+  def challenge
+  end
+
+  def verify
+    @response = Authy::PhoneVerification.check(
+      verification_code: params[:code],
+      country_code: '+375',
+      phone_number: session[:phone_number]
+    )
+    if @response.ok?
+      session[:phone_number] = nil
+      session[:country_code] = nil
+      redirect_to success_phone_verifications_path
+    else
+      render :challenge
+    end
+  end
+
+  def success
+  end
+end
