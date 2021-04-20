1376aa7b95498954b9b10a324c45e8e4a7c3825d
pavel_shymanski <pavl1n>
2021-04-20 20:06:14 +0300

    deleted unnecessary staff

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(_resource)
+    current_user.update(user_type: 1)
     after_signup_index_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,12 +20,12 @@ class User < ApplicationRecord
   def email_required?
     false
   end
-
-  def email_changed?
-    false
-  end
-
-  def will_save_change_to_email?
-    false
-  end
+#
+  # def email_changed?
+  #   false
+  # end
+#
+  # def will_save_change_to_email?
+  #   false
+  # end
 end
