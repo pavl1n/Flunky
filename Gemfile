8c4456eb900209cf5613dc3158a7821a4c8fa828
pavel_shymanski <pavl1n>
2021-04-21 11:33:17 +0300

    reset password with email added:

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jquery-rails', '~> 4.4'
 
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
+gem 'pry', '~> 0.14.1'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Flunky_production"
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password:ENV["GMAIL_PASSWORD"]
+  }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
