3a232358ac85592b9e9a9a8c31f8b82b21bf8de8
pavel_shymanski <pavl1n>
2021-04-22 10:22:14 +0300

    changed old codestyle and deleted one unnecessary gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'menu#index'
   devise_for :users, controllers: { registrations: 'registrations' }
-  match "/404", :to => "errors#not_found", :via => :all
-  match "/500", :to => "errors#internal_server_error", :via => :all
-  resources :phone_verifications, :only => [:new, :create] do |p|
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+  resources :phone_verifications, only: [:new, :create] do |p|
     collection do
       get 'challenge'
       post 'verify'
