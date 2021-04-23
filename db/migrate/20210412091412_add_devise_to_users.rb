791906060403dc652013cd3501790fb356ba69e3
pavel_shymanski <pavl1n>
2021-04-23 12:57:55 +0300

    resolved conflic in unique values of emails

--- db/migrate/20210412091412_add_devise_to_users.rb
+++ db/migrate/20210412091412_add_devise_to_users.rb
@@ -3,7 +3,7 @@
 class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.up
     change_table :users do |t|
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: true
       t.string :encrypted_password, null: false, default: ""
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -63,13 +63,14 @@ ActiveRecord::Schema.define(version: 2021_04_15_061219) do
     t.integer "user_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "email", default: "", null: false
+    t.string "email"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.boolean "confirmed", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
