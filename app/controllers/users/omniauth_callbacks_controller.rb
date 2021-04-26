a260d2529d4ae584aa5da104f632c6d0b0ee556b
pavel_shymanski <pavl1n>
2021-04-26 14:47:11 +0300

    added cop disable

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@
 module Users
   # Controller for omniauth to get data from outside services
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    # rubocop:disable Metrics/AbcSize
     def google_oauth2
       @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user.persisted?
@@ -13,6 +14,7 @@ module Users
         redirect_to new_user_registration_path
       end
     end
+    # rubocop:enable Metrics/AbcSize
 
     def failure
       redirect_to root_path
