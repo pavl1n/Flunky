dc652ae98c901a279aa59f656b5918789b9eda51
pavel_shymanski <pavl1n>
2021-04-28 14:46:12 +0300

    deleted unnecessary styles and added password change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'menu#index'
   devise_for :users, controllers: { registrations: 'client_registrations' }
-
   match '/404', to: 'errors#not_found', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
   resources :phone_verifications, only: %i[new create] do |p|
@@ -13,7 +12,4 @@ Rails.application.routes.draw do
   end
   resources :after_signup
   get 'user_registration/profile'
-  get 'user_registration/password_change'
-  get 'user_registration/delete'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
