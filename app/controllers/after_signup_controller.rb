628ec6f74f68bc8ce718667caddeb325f31df399
pavel_shymanski <pavl1n>
2021-05-02 20:48:45 +0300

    naming fixed

--- app/controllers/after_signup_controller.rb
+++ app/controllers/after_signup_controller.rb
@@ -3,7 +3,7 @@
 # Here describes steps after clients registration
 class AfterSignupController < ApplicationController
   include Wicked::Wizard
-  steps :confirm_profile, :second
+  steps :confirm_profile, :personal_information
 
   def show
     render_wizard
--- app/javascript/packs/redirect.js
+++ app/javascript/packs/redirect.js
@@ -1,3 +1,3 @@
 setTimeout(function(){
-  window.location.href = 'http://localhost:3000/after_signup/second';
+  window.location.href = 'http://localhost:3000/after_signup/personal_information';
 }, 3000);
