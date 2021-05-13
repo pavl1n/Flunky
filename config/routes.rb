cd9f7f08533129e4baac6beaecb8b851448a930f
pavel_shymanski <pavl1n>
2021-05-13 23:08:32 +0300

    added registration through devise for restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   root 'menu#index'
-  devise_for :users, controllers: { registrations: 'client_registrations', omniauth_callbacks: 'users/omniauth_callbacks', passwords: 'passwords' }
+  devise_for :users, controllers: { registrations: 'client_registrations', omniauth_callbacks: 'users/omniauth_callbacks', passwords: 'passwords' }, class_name: 'User'
+  devise_for :restaurants,
+    class_name: 'User',
+    only: [],
+    controllers: { registrations: 'restaurants' }
+
+  devise_scope :restaurant do
+    get   "/restaurants/new", to: "restaurants#new", as: :new_restaurant
+    post  "/restaurants",    to: "restaurants#create", as: :restaurants
+    get  "/restaurants/destroy",    to: "restaurants#destroy", as: :destroy_restaurant_session
+  end
+
   match '/404', to: 'errors#not_found', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
   resources :phone_verifications, only: %i[new create] do
