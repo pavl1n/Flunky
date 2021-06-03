500a0db51f3b1fbedae2dbf9a49c99d8792b1b71
pavel_shymanski <pavl1n>
2021-06-03 10:34:26 +0300

    Updated clients cart to update products and made restriction on order

--- app/controllers/order_positions_controller.rb
+++ app/controllers/order_positions_controller.rb
@@ -4,19 +4,24 @@
 class OrderPositionsController < ApplicationController
   before_action :set_order
   def create
-    @order_positions = @order.order_positions.new(order_params)
-    @order.save
+    if (@order_positions = @order.order_positions.find_by_product_id(params[:order_position][:product_id]))
+      @order_positions.quantity += 1
+      @order_positions.save
+    else
+      @order_positions = @order.order_positions.new(order_params)
+      @order.save
+    end
     session[:order_id] = @order.id
   end
 
   def update
-    @order_positions = @order.order_positions.find(params[:id])
+    @order_positions = @order.order_positions.find_by_id(params[:id])
     @order_positions.update(order_params)
     @order_positions = current_order.order_positions
   end
 
   def destroy
-    @order_positions = @order.order_positions.find(params[:id])
+    @order_positions = @order.order_positions.find_by_id(params[:id])
     @order_positions.destroy
     @order_positions = current_order.order_positions
   end
