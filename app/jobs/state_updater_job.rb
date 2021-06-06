1a13c1b358128a478628c23f033942ec631afc71
pavel_shymanski <pavl1n>
2021-06-06 13:21:48 +0300

    Moved approved to scope

--- app/jobs/state_updater_job.rb
+++ app/jobs/state_updater_job.rb
@@ -5,10 +5,8 @@ class StateUpdaterJob < ApplicationJob
   queue_as :default
 
   def perform
-    Order.where(approved: true, status: %w[waiting in_progress]).each do |order|
-      if order.restaurant_orders.where(approved: true).count == order.restaurant_orders.count
-        order.update(status: 'in_progress')
-      end
+    Order.approved.where(status: %w[waiting in_progress]).each do |order|
+      order.update(status: 'in_progress') if order.restaurant_orders.approved.count == order.restaurant_orders.count
       if order.restaurant_orders.where(status: 'done').count == order.restaurant_orders.count
         order.update(status: 'done')
       end
