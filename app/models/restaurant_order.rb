be00b9af6892d424bb852e0141d227f73f9f84e1
pavel_shymanski <pavl1n>
2021-06-06 13:23:24 +0300

    Added forgotten line to the rest_order model

--- app/models/restaurant_order.rb
+++ app/models/restaurant_order.rb
@@ -2,6 +2,7 @@
 
 # Model which binding orders and restaurants
 class RestaurantOrder < ApplicationRecord
+  scope :approved, -> { where(approved: true) }
   belongs_to :order
   belongs_to :restaurant, -> { where user_type: :restaurant }, class_name: 'User', foreign_key: 'user_id'
   enum status: { waiting: 0, in_progress: 1, done: 2 }
