e497ae567bb98f18e336745d0ee220ac600ce7f9
pavel_shymanski <pavl1n>
2021-06-09 14:13:01 +0300

    Added module

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -9,7 +9,7 @@ class CartController < ApplicationController
 
   def approve
     current_order.update(approved: true)
-    OrderService.new(current_order).restaurant_order
+    OrderService.new(current_order).create_restaurant_order
     redirect_to root_path
     flash[:notice] = 'Order was succesfully created'
   end
