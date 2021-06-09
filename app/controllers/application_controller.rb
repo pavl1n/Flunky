29815056b9e6b915d818b6858bbca3dcf0386e15
pavel_shymanski <pavl1n>
2021-06-09 15:33:04 +0300

    Connected orders to clients and added comments for module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 # This module smells of :reek:IrresponsibleModule
 class ApplicationController < ActionController::Base
+  before_action :join_order_to_client, if: -> { current_order.client_id.nil? }
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def current_order
@@ -13,5 +14,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:phone_number])
+  end
+
+  def join_order_to_client
+    current_order.update(client_id: current_user.id) if user_signed_in? && !current_order.nil?
   end
 end
--- app/helpers/restaurant_order_helper.rb
+++ app/helpers/restaurant_order_helper.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: false
+
+# Helper for separating products
 module RestaurantOrderHelper
   def find_products_name(order)
     order.order.products.select { |product| current_user.id == product.restaurant.id }.map(&:name)
