40cade56b8a02811f9c8552f09dfbac3b29aa6d5
pavel_shymanski <pavl1n>
2021-06-09 21:15:12 +0300

    Codestyle issues fixed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: false
+
+# Controller to show each user products
 class OrdersController < ApplicationController
   def index
     @orders = Order.where(client_id: current_user.id)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: false
+
+# Join's each order to user after login
 class SessionsController < Devise::SessionsController
   after_action :after_login, only: :create, unless: -> { Order.find_by_id(session[:order_id]).nil? }
   def after_login
