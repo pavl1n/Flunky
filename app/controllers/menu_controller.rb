acf1897d2c7f6c36be9bef00bd978bc34effffb3
pavel_shymanski <pavl1n>
2021-06-13 18:44:51 +0300

    killed n+1 quaries

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -4,6 +4,6 @@
 class MenuController < ApplicationController
   before_action :init_cart
   def index
-    @pagy, @product = pagy(Product.all)
+    @pagy, @product = pagy(Product.all.includes([:restaurant]).includes(product_picture_attachment: :blob))
   end
 end
