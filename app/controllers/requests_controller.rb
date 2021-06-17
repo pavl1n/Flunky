a061ab64ceeb2a15523a908607c602380c82b427
pavel_shymanski <pavl1n>
2021-06-17 13:33:22 +0300

    fully done mail sender

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: false
+
+# Responsible for requests from replying form
 class RequestsController < ApplicationController
   def index; end
-
-  def show; end
 
   def new
     @request = Request.new
@@ -11,6 +12,7 @@ class RequestsController < ApplicationController
     @request = Request.new(request_params)
     if @request.save
       redirect_to root_path
+      RestaurantRequestMailer.welcome_email(request_params[:email]).deliver_now
       flash[:notice] = 'Request accepted'
     else
       render :new
