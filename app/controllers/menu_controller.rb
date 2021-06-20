c301b74b2739db4c8834df1ec1a1a7b5c9d69387
pavel_shymanski <pavl1n>
2021-06-20 13:08:30 +0300

    fixed bugs with bullet and displaying

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -5,7 +5,7 @@ class MenuController < ApplicationController
   include Constants
   before_action :init_cart
   def index
-    @product = admin(current_user) ? Product.all : Product.approved
+    @product = admin(current_user) ? Product.all : Product.approved.joins(:restaurant).where('users.confirmed is TRUE')
     @pagy, @products = pagy(@product.includes([:restaurant]).includes(product_picture_attachment: :blob), items: ITEMS)
   end
 end
